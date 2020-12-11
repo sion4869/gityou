e3aca0aa04fd1c5cec38f5eafa6b7887309a4f7e
sion4869 <unicorn4869no.5@gmail.com>
2020-12-11 16:56:42 +0900

    Lです

--- gityou/git.cpp
+++ gityou/git.cpp
@@ -4,6 +4,6 @@ int main() {
 	std::cout << "Hello World!" << std::endl;
 	std::cout << "Let's commit!" << std::endl;
 	std::cout << "Now branch is Master!" << std::endl;
-	std::cout << "Now branch is Kari!" << std::endl;
+	std::cout << "Now branch is Ldesu!" << std::endl;
 	return 0;
 }
