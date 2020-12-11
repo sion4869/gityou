774a6ed6e9e53bfcb0cf52849da157cd7790be87
sion4869 <unicorn4869no.5@gmail.com>
2020-12-11 13:33:18 +0900

    ソース作成

---
+++ gityou/git.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Let's commit!" << std::endl;
+	std::cout << "Now branch is master!" << std::endl;
+	return 0;
+}
